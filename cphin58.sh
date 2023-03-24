98a3a34b67265a2fa40044b511a0240ae7606d20
zava810 <zinglish@kakao.com>
2023-03-25 00:58:57 +0530

    fb.com/ztrabc tvitter.com/etphor

---
+++ cphin58.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo rm /home/viml/.config/GIMP/2.10/fonts/hin5*.ttf
+sudo rm /home/viml/.fonts/hin5*.ttf
+sudo rm /home/viml/.local/share/fonts/hin5*.ttf
+sudo rm /home/viml/.local/share/fonts/Monotype/hin5*.ttf
+sudo rm /usr/local/share/fonts/hin5*.ttf
+sudo rm /usr/share/fonts/truetype/hscii/hin5*.ttf
+sudo rm /usr/share/gimp/2.0/fonts/hin5*.ttf
+#############
+sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/mg/free/font/
+sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/mg/free/font/
+#############
+sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.config/GIMP/2.10/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.local/share/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.local/share/fonts/Monotype/
+sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/share/fonts/truetype/hscii/
+sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/local/share/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/share/gimp/2.0/fonts/
+# 54
+sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.local/share/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.local/share/fonts/Monotype/
+sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.config/GIMP/2.10/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/share/fonts/truetype/hscii/
+sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/local/share/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/share/gimp/2.0/fonts/
+sudo fc-cache -fv
+fc-cache -fv
+exit
+fc-cache -fv
