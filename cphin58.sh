3c961769184955fb0b1209a7965d8d2d2f72716b
zava810 <zinglish@kakao.com>
2023-03-25 04:59:49 +0530

    fb.com/ztrabc tvitter.com/etphor

--- cphin58.sh
+++ cphin58.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo rm /home/viml/.config/GIMP/2.10/fonts/hin5*.ttf
-sudo rm /home/viml/.fonts/hin5*.ttf
-sudo rm /home/viml/.local/share/fonts/hin5*.ttf
-sudo rm /home/viml/.local/share/fonts/Monotype/hin5*.ttf
-sudo rm /usr/local/share/fonts/hin5*.ttf
-sudo rm /usr/share/fonts/truetype/hscii/hin5*.ttf
-sudo rm /usr/share/gimp/2.0/fonts/hin5*.ttf
+#sudo rm /home/viml/.config/GIMP/2.10/fonts/hin5*.ttf
+#sudo rm /home/viml/.fonts/hin5*.ttf
+#sudo rm /home/viml/.local/share/fonts/hin5*.ttf
+#sudo rm /home/viml/.local/share/fonts/Monotype/hin5*.ttf
+#sudo rm /usr/local/share/fonts/hin5*.ttf
+#sudo rm /usr/share/fonts/truetype/hscii/hin5*.ttf
+#sudo rm /usr/share/gimp/2.0/fonts/hin5*.ttf
 #############
 sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/mg/free/font/
-sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/mg/free/font/
+sudo cp /home/viml/mg/font/sfd/5/hin58m.ttf /home/viml/mg/free/font/
 #############
 sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.config/GIMP/2.10/fonts/
 sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.fonts/
@@ -18,13 +18,13 @@ sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/share/fonts/truetype/hscii/
 sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/local/share/fonts/
 sudo cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/share/gimp/2.0/fonts/
 # 54
-sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.fonts/
-sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.local/share/fonts/
-sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.local/share/fonts/Monotype/
-sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.config/GIMP/2.10/fonts/
-sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/share/fonts/truetype/hscii/
-sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/local/share/fonts/
-sudo cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/share/gimp/2.0/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58m.ttf /home/viml/.fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58m.ttf /home/viml/.local/share/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58m.ttf /home/viml/.local/share/fonts/Monotype/
+sudo cp /home/viml/mg/font/sfd/5/hin58m.ttf /home/viml/.config/GIMP/2.10/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58m.ttf /usr/share/fonts/truetype/hscii/
+sudo cp /home/viml/mg/font/sfd/5/hin58m.ttf /usr/local/share/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin58m.ttf /usr/share/gimp/2.0/fonts/
 sudo fc-cache -fv
 fc-cache -fv
 exit
