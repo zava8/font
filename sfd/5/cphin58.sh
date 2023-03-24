31b8c9646a595cc970937ac23f0a1e1a8d1c3c7f
zava810 <zinglish@kakao.com>
2023-03-24 13:19:59 +0530

    fb.com/ztrabc tvitter.com/etphor

---
+++ sfd/5/cphin58.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.fonts/
+cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/mg/free/font/
+cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.local/share/fonts/
+cp /home/viml/mg/font/sfd/5/hin58.ttf /home/viml/.config/GIMP/2.10/fonts/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.fonts/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/mg/free/font/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.local/share/fonts/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /home/viml/.config/GIMP/2.10/fonts/
+fc-cache -fv
+sudo -i
+cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/share/fonts/truetype/hscii/
+cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/local/share/fonts/
+cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/share/gimp/2.0/fonts/
+cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/share/fonts/truetype/hscii/
+cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/local/share/fonts/
+cp /home/viml/mg/font/sfd/5/hin58.ttf /usr/share/gimp/2.0/fonts/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/share/fonts/truetype/hscii/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/local/share/fonts/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/share/gimp/2.0/fonts/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/share/fonts/truetype/hscii/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/local/share/fonts/
+cp /home/viml/mg/font/sfd/5/hin54.ttf /usr/share/gimp/2.0/fonts/
