065e05f469fa182ac0c2adafedb20339b751c04c
zava810 <zinglish@kakao.com>
2023-03-25 18:27:08 +0530

    fb.com/ztrabc tvitter.com/etphor

--- cphin58.sh
+++ cphin58.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+echo "cp to free" 
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/free/font/
 #############
+echo "cp to gimp2" 
+sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/gimp/2.0/fonts/
+echo "cp to gimp210" 
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.config/GIMP/2.10/fonts/
+echo "cp to .fonts" 
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.fonts/
+echo "cp to ..local/share/fonts" 
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.local/share/fonts/
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.local/share/fonts/Monotype/
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/fonts/truetype/hscii/
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/local/share/fonts/
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/gimp/2.0/fonts/
 # 54
 sudo fc-cache -fv
 fc-cache -fv
