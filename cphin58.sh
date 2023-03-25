71d1b397a3ed31b737f47931f98b097eae37a57f
zava810 <zinglish@kakao.com>
2023-03-26 01:27:12 +0530

    fb.com/ztrabc tvitter.com/etphor

--- cphin58.sh
+++ cphin58.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-echo "cp to free" 
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/free/font/
-#############
-echo "cp to gimp2" 
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/gimp/2.0/fonts/
-echo "cp to gimp210" 
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.config/GIMP/2.10/fonts/
-echo "cp to .fonts" 
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.fonts/
-echo "cp to ..local/share/fonts" 
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.local/share/fonts/
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.local/share/fonts/Monotype/
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/fonts/truetype/hscii/
-sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/local/share/fonts/
-# 54
-sudo fc-cache -fv
+#############################
+cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/free/font/
+cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/dounload/font/5/
+cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/font/daunlod/5/
+cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/hpop/app/src/main/res/font/
+cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/si_hpop/app/src/main/res/font/
+#############################
+cp /home/viml/mg/font/sfd/5/hin5*m.ttf /home/viml/.local/share/fonts/Monotype/
+cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.local/share/fonts/
+#############################
 fc-cache -fv
+#############################
+sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/local/share/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/fonts/truetype/hscii/
+sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/gimp/2.0/fonts/
+sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.config/GIMP/2.10/fonts/
+#############################
+sudo fc-cache -fv
+#############################
 git add . ; git commit -am "fb.com/ztrabc tvitter.com/etphor" ; git push
 cd ~/mg/free/
 git add . ; git commit -am "fb.com/ztrabc tvitter.com/etphor" ; git push
+###### D end ###############
