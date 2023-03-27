3b9302535ca27fc9c3db994c308af4f724b5c330
zava810 <zinglish@kakao.com>
2023-03-27 12:35:25 +0530

    fb.com/ztrabc tvitter.com/etphor

--- cphin.sh
+++ cphin.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 #############################
+read -p "old hinDi fonts hin*.ttf remove? [y,n]" rm_kro_kya 
+case $rm_kro_kya in  
+  y|Y)
+     echo "yes old hinDi fonts hin*.ttf removing starts"
+     rm /home/viml/.local/share/fonts/Monotype/hin5*.ttf
+     rm /home/viml/.local/share/fonts/hin5*.ttf
+     sudo rm /home/viml/.fonts/hin5*.ttf 
+     sudo rm /usr/local/share/fonts/hin5*.ttf 
+     sudo rm /usr/share/fonts/truetype/hscii/hin5*.ttf 
+     sudo rm /usr/share/gimp/2.0/fonts/hin5*.ttf 
+     sudo rm /home/viml/.config/GIMP/2.10/fonts/hin5*.ttf 
+     echo yes old hinDi fonts hin*.ttf removing ends
+     ;; 
+  n|N) echo no ;; 
+  *) echo dont know ;; 
+esac
+############################
 cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/free/font/
 cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/dounload/font/5/
 cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/font/daunlod/5/
 cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/hpop/app/src/main/res/font/
 cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/mg/si_hpop/app/src/main/res/font/
-#############################
 cp /home/viml/mg/font/sfd/5/hin5*m.ttf /home/viml/.local/share/fonts/Monotype/
 cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.local/share/fonts/
-#############################
-fc-cache -fv
 #############################
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.fonts/
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/local/share/fonts/
@@ -17,6 +31,7 @@ sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/fonts/truetype/hscii/
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /usr/share/gimp/2.0/fonts/
 sudo cp /home/viml/mg/font/sfd/5/hin5*.ttf /home/viml/.config/GIMP/2.10/fonts/
 #############################
+fc-cache -fv
 sudo fc-cache -fv
 #############################
 git add . ; git commit -am "fb.com/ztrabc tvitter.com/etphor" ; git push
