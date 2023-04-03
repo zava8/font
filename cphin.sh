635a20965d5e83e5f79be1968c572d74b63f9535
zava810 <zinglish@kakao.com>
2023-04-03 09:34:45 +0530

    fb.com/ztrabc tvitter.com/etphor

--- cphin.sh
+++ cphin.sh
@@ -56,24 +56,9 @@ system font chenz to :
 2. hin58 fontsize // hin58 12
 3. hin5i fontsize // hin5i 12
 "
-printf "${text_content}"
 read -p "system font chenz yes no reset ? [y,n,r]" systemfont_cenz_to
 printf "systemfont_cenz_to ki value ${systemfont_cenz_to} h."
-read -p "system font chenz yes no reset ? [y,n,r]" systemfont_cenz_kro_kya 
-case $systemfont_cenz_kro_kya in  
-  y|Y)
-   gsettings set org.cinnamon.desktop.interface font-name 'hin58 12'
-   gsettings set org.gnome.desktop.interface font-name 'hin58 12'
-   gsettings set org.gnome.desktop.interface document-font-name 'hin58 12'
-   gsettings set org.gnome.desktop.interface monospace-font-name 'hin58m 12'
-   gsettings set org.cinnamon.desktop.wm.preferences titlebar-uses-system-font false
-   gsettings set org.cinnamon.desktop.wm.preferences titlebar-font 'hin58 12'
-   gsettings set org.gnome.desktop.wm.preferences titlebar-uses-system-font false
-   gsettings set org.gnome.desktop.wm.preferences titlebar-font 'hin58 10'
-   gsettings set org.x.editor.preferences.editor use-default-font false
-   gsettings set org.x.editor.preferences.editor editor-font 'hin58 14'
-   gsettings set org.nemo.desktop font 'hin58 12'
-     ;;
+case $systemfont_cenz_to in  
   r|R)
    gsettings reset org.cinnamon.desktop.interface font-name
    gsettings reset org.gnome.desktop.interface font-name
@@ -86,6 +71,19 @@ case $systemfont_cenz_kro_kya in
    gsettings reset org.x.editor.preferences.editor use-default-font
    gsettings reset org.x.editor.preferences.editor editor-font
    gsettings reset org.nemo.desktop font
+     ;;
+  *)
+   gsettings set org.cinnamon.desktop.interface font-name $systemfont_cenz_to
+   gsettings set org.gnome.desktop.interface font-name $systemfont_cenz_to
+   gsettings set org.gnome.desktop.interface document-font-name $systemfont_cenz_to
+   gsettings set org.gnome.desktop.interface monospace-font-name $systemfont_cenz_to
+   gsettings set org.cinnamon.desktop.wm.preferences titlebar-uses-system-font false
+   gsettings set org.cinnamon.desktop.wm.preferences titlebar-font $systemfont_cenz_to
+   gsettings set org.gnome.desktop.wm.preferences titlebar-uses-system-font false
+   gsettings set org.gnome.desktop.wm.preferences titlebar-font $systemfont_cenz_to
+   gsettings set org.x.editor.preferences.editor use-default-font false
+   gsettings set org.x.editor.preferences.editor editor-font $systemfont_cenz_to
+   gsettings set org.nemo.desktop font $systemfont_cenz_to
      ;;
 esac
 echo "nao system fonts h:"
